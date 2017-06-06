7e44e98224d293966076ecf8534bc1a0f1d41e64
Joginaidu Gopisetti <jgopisetti@corp.innominds.com>
2017-06-06 06:21:32 +0530

    After Profile Pic

--- src/app/theme/components/baPageTop/baPageTop.component.ts
+++ src/app/theme/components/baPageTop/baPageTop.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component,OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {GlobalState} from '../../../global.state';
 import {AppService} from '../../../app.service'; 
@@ -9,8 +9,8 @@ import {AppService} from '../../../app.service';
   templateUrl: './baPageTop.html',
   styleUrls: ['./baPageTop.scss']
 })
-export class BaPageTop {
-
+export class BaPageTop implements OnInit{
+image_path ;
   public isScrolled:boolean = false;
   public isMenuCollapsed:boolean = false;
 
@@ -19,7 +19,12 @@ export class BaPageTop {
       this.isMenuCollapsed = isCollapsed;
     });
   }
-
+ngOnInit(){
+  var user = JSON.parse(localStorage.getItem('user'));
+  console.log(user);
+  this.image_path = "http://localhost:8080/images/"+ user[0].empImage;
+  console.log(this.image_path);
+}
   public toggleMenu() {
     this.isMenuCollapsed = !this.isMenuCollapsed;
     this._state.notifyDataChanged('menu.isCollapsed', this.isMenuCollapsed);
